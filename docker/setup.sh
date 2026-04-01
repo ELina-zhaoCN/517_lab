6deb367dd7d832032e2a540ee4257ea7952b51e7
jack <42076119+htchr@users.noreply.github.com>
2026-03-31 17:23:52 -0700

    remove references to soa_ws

--- docker/setup.sh
+++ docker/setup.sh
@@ -55,6 +55,6 @@ pip uninstall torch torchvision torchaudio -y
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
 
 # build ros
-cd /home/ubuntu/techin517/soa_ws/src/
+cd /home/ubuntu/techin517/ros2_ws/src/
 bash /home/ubuntu/techin517/ros2_ws/src/soa_ros2/build.sh
 source /home/ubuntu/.bashrc
